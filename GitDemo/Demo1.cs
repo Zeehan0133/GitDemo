816ac289706271e9beb5c5f1906dccec39b5f29e
Zeehan0133 <zyne0133@gmail.com>
2021-10-29 09:27:45 +0530

    this is class1 file

---
+++ GitDemo/Demo1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitDemo
+{
+    class Demo1
+    {
+        public static void Demo()
+        {
+            Console.WriteLine("this is Demo1111111111111");
+
+        }
+        
+      
+        
+    }
+}
